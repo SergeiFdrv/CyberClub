625c928811dab072412fd7f69f918f72de39f7eb
Sergei F <51461227+SergeiFdrv@users.noreply.github.com>
2020-09-27 06:34:25 +0300

    Update the query for creating the DB

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -14,11 +14,34 @@ CREATE TABLE games (gameid INT PRIMARY KEY IDENTITY, gamename NVARCHAR(50) NOT N
 madeby INT FOREIGN KEY REFERENCES devs (devid), gamelink NVARCHAR(255), 
 gamepic INT FOREIGN KEY REFERENCES pics (picid), singleplayer BIT, multiplayer BIT)
 CREATE TABLE genres (genreid INT PRIMARY KEY IDENTITY, genrename NVARCHAR(30) NOT NULL UNIQUE)
-CREATE TABLE gamegenre (game INT FOREIGN KEY REFERENCES games (gameid), 
-genre INT FOREIGN KEY REFERENCES genres (genreid), PRIMARY KEY (game, genre))
-CREATE TABLE subscriptions (who INT FOREIGN KEY REFERENCES users (userid), 
-game INT FOREIGN KEY REFERENCES games (gameid), rate tinyint check(rate >= 0 AND rate <= 10), 
-PRIMARY KEY (who, game))
+CREATE TABLE gamegenre (game INT FOREIGN KEY REFERENCES games (gameid) ON DELETE CASCADE, 
+genre INT FOREIGN KEY REFERENCES genres (genreid) ON DELETE CASCADE, PRIMARY KEY (game, genre))
+CREATE TABLE subscriptions (who INT FOREIGN KEY REFERENCES users (userid) ON DELETE CASCADE, 
+game INT FOREIGN KEY REFERENCES games (gameid) ON DELETE CASCADE,
+rate tinyint check(rate >= 0 AND rate <= 10), PRIMARY KEY (who, game))
+-- триггеры
+GO
+CREATE TRIGGER Devs_GamesForget
+ON devs
+INSTEAD OF DELETE
+AS
+    UPDATE games SET madeby = null WHERE madeby IN (SELECT devid FROM deleted)
+    DELETE FROM devs WHERE devid IN (SELECT devid FROM deleted)
+GO
+CREATE TRIGGER Pics_GamesForget
+ON pics
+INSTEAD OF DELETE
+AS
+    UPDATE games SET gamepic = null WHERE gamepic IN (SELECT picid FROM deleted)
+    DELETE FROM pics WHERE picid IN (SELECT picid FROM deleted)
+GO/*
+CREATE TRIGGER Users_FeedbackForget
+ON users
+INSTEAD OF DELETE
+AS
+    UPDATE feedback SET who = null WHERE who IN (SELECT who FROM deleted)
+    DELETE FROM users WHERE userid IN (SELECT userid FROM deleted)
+*/GO
 -- добавить начальные данные
 INSERT hierarchy VALUES ('admin'), ('player'), ('banned')
 INSERT users (username, authority, passwd) VALUES ('admin', 1, '')
